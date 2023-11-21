8ddbc724ef2f91635d4f19186888bc942aafc1ba
NilubaJashanaS <134999786+NilubaJashanaS@users.noreply.github.com>
2023-11-21 11:19:02 +0530

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import {registerLicense} from '@syncfusion/ej2-base';
+registerLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cWWNCf1JpRGNGfV5yd0VAalxRTnJZUiweQnxTdEZiWX5YcHxQRGRfV0Z3XA==");
+createApp(App).mount('#app')
